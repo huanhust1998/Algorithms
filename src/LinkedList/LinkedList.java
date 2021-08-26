c798fbb0076e3da5b41c1b76780f5d7110c8e8a8
Xuan Huan Ninh <huannx@mintpotvn.com>
2021-08-26 22:02:10 +0700

    update function delete node

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -14,73 +14,128 @@ public class LinkedList {
         }
     }
 
-    public void printList() {
+    public void printNode() {
         Node n = head;
         while (n != null) {
-            System.out.println(n.data + " " + n);
+            System.out.println(n.data + "  " + n);
             n = n.next;
         }
     }
 
-    //thêm vào đầu danh sách liên kết
+    //I. Chèn phần tử vào danh sách
+    //thêm phần tử vào đầu danh sách
     public void push(int new_data) {
         Node new_node = new Node(new_data);
-        // cho node mới trỏ tới node đầu tiên
+
         new_node.next = head;
-        //cập nhật node đầu bằng node mới
+
         head = new_node;
+        //head.next = new_node;
     }
 
-    //thêm node mới vào sau một node cụ thể
-    public void insertAfter(Node prev_node, int new_data) {
-        /*1. Check if the given Node is null*/
-        if (prev_node == null) {
+    //thêm vào sau một node
+    public void insertAfter(Node node, int new_data) {
+        if (node == null) {
             System.out.println("The given previous node cannot be null");
             return;
         }
         Node new_node = new Node(new_data);
 
-        // node mới trỏ tới node sau node hiện tại
-        new_node.next = prev_node.next;
+        new_node.next = node.next;
 
-        //node hiện tại trỏ tới node mới
-        prev_node.next = new_node;
+        node.next = new_node;
     }
 
-    /*//thêm node mới vào trước một node cụ thể
-    public void insertBefore(Node prev_node, int new_data) {
-        if (prev_node == null) {
+    // thêm vào trước một node
+    public void insertBefore(Node node, int new_data) {
+        if (node == null) {
             System.out.println("The given previous node cannot be null");
             return;
         }
+
         Node new_node = new Node(new_data);
 
-        //lấy ra node trước node hiện tại
-        Node before_Node = null;
-        before_Node.next = prev_node;
+        new_node.next = node;
 
-        System.out.println("before_Node: " + before_Node);
-        before_Node.next = new_node;
-        new_node.next = prev_node;
-    }*/
+        Node n = head;
+
+        while (n.next != node) {
+            if (n.next == node)
+                break;
+            n = n.next;
+        }
+        n.next = new_node;
+        return;
+    }
 
     //thêm vào cuối danh sách
+    public void append(int new_data) {
+        Node new_node = new Node(new_data);
+        Node n = head;
+        if (n == null) {
+            head = new Node(new_data);
+        } else {
+            while (n.next != null) {
+                n = n.next;
+            }
+        }
+        new_node.next = null;
+        n.next = new_node;
+        return;
+    }
+
+    // II. Xóa phần tử khỏi danh sách
+    // xóa ở đầu danh sách
+    public void deleteHead() {
+        Node temp = head;
+        if (temp != null) {
+            head = temp.next;
+        }
+    }
+
+    //xóa ở cuối danh sách
+    public void pop() {
+        Node last = head;
+        while (last.next.next != null) {
+            last = last.next;
+        }
+        last.next = null;
+    }
+
+    //xóa phần tử cụ thể
+    public void deleteNode(int data) {
+        Node temp = head, prev = null;
+        if (temp != null && temp.data == data) {
+            head = temp.next;
+            return;
+        }
+        //tìm ra node
+        while (temp != null) {
+            if (temp.data == data) break;
+            prev = temp;
+            temp = temp.next;
+        }
+
+        prev.next = temp.next;
+
+    }
 
     public static void main(String[] args) {
-        // write your code here
         LinkedList linkedList = new LinkedList();
+
         linkedList.head = new Node(1);
-        Node second = new Node(2);
-        Node third = new Node(3);
+        Node first = new Node(2);
+        Node second = new Node(3);
 
-        linkedList.head.next = second;
+        linkedList.head.next = first;
+        first.next = second;
 
-        second.next = third;
-
-        linkedList.push(100);
-        linkedList.insertAfter(second, 1000);
-        //linkedList.insertBefore(third, 1000);
-
-        linkedList.printList();
+        linkedList.push(123);
+        linkedList.insertAfter(first, 100);
+        linkedList.insertBefore(first, 1000);
+        linkedList.append(100000);
+        linkedList.deleteHead();
+        linkedList.deleteNode(2);
+        linkedList.printNode();
     }
 }

e2adf9e1710bfbe79a0491a0afe050f05f6e707c
Xuan Huan Ninh <huannx@mintpotvn.com>
2021-08-31 14:14:33 +0700

    create circularLinkedList

--- src/LinkedList/CircularLinkedList.java
+++ src/LinkedList/CircularLinkedList.java
@@ -1,8 +1,46 @@
 package LinkedList;
 
 public class CircularLinkedList {
-    public static void main(String[] args) {
 
+    static class Node {
+        int data;
+        Node next;
+    }
+
+    public static Node push(Node head_ref, int data) {
+        Node prev = new Node();
+        Node temp = head_ref;
+        prev.data = data;
+        prev.next = head_ref;
+
+        if (head_ref != null) {
+            while (temp.next != head_ref) {
+                temp = temp.next;
+            }
+            temp.next = prev;
+        } else {
+            prev.next = prev;
+        }
+        head_ref = prev;
+        return head_ref;
+    }
+
+    public static void printNode(Node head) {
+        Node temp = head;
+        do {
+            System.out.println(temp.data + " :" + temp);
+            temp = temp.next;
+        } while (temp != head);
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+
+        head = push(head, 1234);
+        head = push(head, 12345);
+        head = push(head, 12346);
+        head = push(head, 12347);
+        printNode(head);
     }
 }
 
